ba7e06e27b87f8d882da3238cad0c463cf0bb7e5
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-02-14 19:35:22 -0500

    replace PortNumber.h by OnlMonDefs.h

---
+++ onlmonserver/OnlMonDefs.h
@@ -0,0 +1,12 @@
+#ifndef ONLMONSERVER_ONLMONDEFS_H
+#define ONLMONSERVER_ONLMONDEFS_H
+
+namespace OnlMonDefs
+{
+// Port to use by framework
+  const unsigned int MONIPORT = 9081;
+  const unsigned int NUMMONIPORT = 5;
+  const unsigned int MSGLEN = 256;
+}
+
+#endif
--- onlmonserver/PortNumber.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PORTNUMBER_H__
-#define __PORTNUMBER_H__
-
-// Port to use by framework
-#define MONIPORT 9081
-#define NUMMONIPORT 5
-#endif
