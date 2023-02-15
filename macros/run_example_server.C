76d1a1e5fbd6fa9c2cac80ea1b420ff8c644f616
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-02-14 19:42:37 -0500

    Add server id setting

--- macros/run_example_server.C
+++ macros/run_example_server.C
@@ -6,10 +6,10 @@
 
 R__LOAD_LIBRARY(libonlmymon_server.so)
 
-void run_example_server(const char *prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
+void run_example_server(const std::string &name = "MYMON", unsigned int serverid = 0, const std::string &prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
 {
-  OnlMon *m = new MyMon();                      // create subsystem Monitor object
-                                                //  m->AddTrigger("PPG(Laser)");  // high efficiency triggers selection at et pool
+  OnlMon *m = new MyMon(name);                      // create subsystem Monitor object
+  m->SetServerId(serverid);                                                //  m->AddTrigger("PPG(Laser)");  // high efficiency triggers selection at et pool
                                                 //  m->AddTrigger("ONLMONBBCLL1"); // generic bbcll1 minbias trigger (defined in ServerFuncs.C)
   OnlMonServer *se = OnlMonServer::instance();  // get pointer to Server Framework
   se->registerMonitor(m);                       // register subsystem Monitor with Framework
