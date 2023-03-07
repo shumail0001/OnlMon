ad0ab431315fb7aeb3ceefe167db3bb020146efb
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-03-07 11:26:17 -0500

    adjust epd to use multiple servers

--- macros/run_epd_client.C
+++ macros/run_epd_client.C
@@ -11,34 +11,34 @@ void epdDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
   // register histos we want with monitor name
-  cl->registerHisto("epdmon_hist1", "EPDMON");
-  cl->registerHisto("epdmon_hist2", "EPDMON");
+  cl->registerHisto("epdmon_hist1", "EPDMON_0");
+  cl->registerHisto("epdmon_hist2", "EPDMON_0");
   cl->AddServerHost("localhost");  // check local host first
   CreateHostList(online);
   // get my histos from server, the second parameter = 1
   // says I know they are all on the same node
-  cl->requestHistoBySubSystem("EPDMON", 1);
-  OnlMonDraw *epdmon = new EpdMonDraw();  // create Drawing Object
+  cl->requestHistoBySubSystem("EPDMON_0", 1);
+  OnlMonDraw *epdmon = new EpdMonDraw("EPDMONDRAW");  // create Drawing Object
   cl->registerDrawer(epdmon);             // register with client framework
 }
 
 void epdDraw(const char *what = "ALL")
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->requestHistoBySubSystem("EPDMON");        // update histos
-  cl->Draw("EPDMON", what);                     // Draw Histos of registered Drawers
+  cl->requestHistoBySubSystem("EPDMON_0");        // update histos
+  cl->Draw("EPDMONDRAW", what);                     // Draw Histos of registered Drawers
 }
 
 void epdPS()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakePS("EPDMON");                         // Create PS files
+  cl->MakePS("EPDMONDRAW");                         // Create PS files
   return;
 }
 
 void epdHtml()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakeHtml("EPDMON");                       // Create html output
+  cl->MakeHtml("EPDMONDRAW");                       // Create html output
   return;
 }
--- macros/run_epd_server.C
+++ macros/run_epd_server.C
@@ -6,9 +6,10 @@
 
 R__LOAD_LIBRARY(libonlepdmon_server.so)
 
-void run_epd_server(const char *prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
+void run_epd_server(const std::string &name = "EPDMON", unsigned int serverid = 0, const std::string &prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
 {
-  OnlMon *m = new EpdMon();                     // create subsystem Monitor object
+  OnlMon *m = new EpdMon(name);                     // create subsystem Monitor object
+  m->SetMonitorServerId(serverid);
                                                 //  m->AddTrigger("PPG(Laser)");  // high efficiency triggers selection at et pool
                                                 //  m->AddTrigger("ONLMONBBCLL1"); // generic bbcll1 minbias trigger (defined in ServerFuncs.C)
   OnlMonServer *se = OnlMonServer::instance();  // get pointer to Server Framework
