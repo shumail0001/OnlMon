94d7a0671e0a1fa0c4a90cc9aecbf331d943fe6c
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-03-07 11:53:18 -0500

    adjust intt to use multiple servers

--- macros/run_intt_client.C
+++ macros/run_intt_client.C
@@ -7,25 +7,6 @@
 // cppcheck-suppress unknownMacro
 R__LOAD_LIBRARY(libonlinttmon_client.so)
 
-void run_intt_client()
-{
-
-  OnlMonClient *cl = OnlMonClient::instance();
-  // register histos we want with monitor name
-
-  cl->registerHisto("InttNumEvents", "INTTMON");
-  cl->registerHisto("InttHitMap", "INTTMON");
-  cl->registerHisto("InttHitMapRef", "INTTMON");
-
-  cl->AddServerHost("localhost");   // check local host first
-  CreateHostList(0);
-  // get my histos from server, the second parameter = 1
-  // says I know they are all on the same node
-  cl->requestHistoBySubSystem("INTTMON");
-  OnlMonDraw* inttmondraw = new InttMonDraw();
-  cl->registerDrawer(inttmondraw);              // register with client framework
-}
-
 void inttDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
@@ -34,10 +15,13 @@ void inttDrawInit(const int online = 0)
 
   cl->AddServerHost("localhost");   // check local host first
   CreateHostList(online);
+   cl->registerHisto("InttNumEvents", "INTTMON_0");
+   cl->registerHisto("InttHitMap", "INTTMON_0");
+   cl->registerHisto("InttHitMapRef", "INTTMON_0");
   // get my histos from server, the second parameter = 1
   // says I know they are all on the same node
-  cl->requestHistoBySubSystem("INTTMON", 1);
-  OnlMonDraw *inttmon = new InttMonDraw();    // create Drawing Object
+  cl->requestHistoBySubSystem("INTTMON_0", 1);
+  OnlMonDraw *inttmon = new InttMonDraw("INTTMONDRAW");    // create Drawing Object
 	inttmon->Init(); //registers the hists it will need to the OnlMonClient
   cl->registerDrawer(inttmon);              // register with client framework
 }
@@ -45,20 +29,20 @@ void inttDrawInit(const int online = 0)
 void inttDraw(const char *what="ALL")
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->requestHistoBySubSystem("INTTMON");         // update histos
-  cl->Draw("INTTMON",what);                       // Draw Histos of registered Drawers
+  cl->requestHistoBySubSystem("INTTMON_0");         // update histos
+  cl->Draw("INTTMONDRAW",what);                       // Draw Histos of registered Drawers
 }
 
 void inttPS()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakePS("INTTMON");                          // Create PS files
+  cl->MakePS("INTTMONDRAW");                          // Create PS files
   return;
 }
 
 void inttHtml()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakeHtml("INTTMON");                        // Create html output
+  cl->MakeHtml("INTTMONDRAW");                        // Create html output
   return;
 }
--- macros/run_intt_server.C
+++ macros/run_intt_server.C
@@ -7,19 +7,24 @@
 // cppcheck-suppress unknownMacro
 R__LOAD_LIBRARY(libonlinttmon_server.so)
 
-void run_intt_server(const char *prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
+void run_intt_server(const std::string &name = "INTTMON", unsigned int serverid = 0, const std::string &prdffile = "/sphenix/data/data02/sphenix/t1044/rcdaq-00000221-0000.prdf")
 {
 //  //for debugging
 //  InttMon *m = new InttMon();
 //  m->MiscDebug();
 
-  OnlMon *m = new InttMon();      // create subsystem Monitor object
-//  m->AddTrigger("PPG(Laser)");  // high efficiency triggers selection at et pool
+  OnlMon *m = new InttMon(name);      // create subsystem Monitor object
+  m->SetMonitorServerId(serverid);
 //  m->AddTrigger("ONLMONBBCLL1"); // generic bbcll1 minbias trigger (defined in ServerFuncs.C)
   OnlMonServer *se = OnlMonServer::instance(); // get pointer to Server Framework
   se->registerMonitor(m);       // register subsystem Monitor with Framework
   start_server(prdffile);
 
+//**********************************************************
+//
+// just a comment: This is not how this will be run
+//
+//**********************************************************
   //m->Init(); this must already be called somewhere above
   m->BeginRun(1);
 
