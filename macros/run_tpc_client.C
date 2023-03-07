7fddb2c7f74d5bf5fc3debf85da2730b6f96b918
Chris Pinkenburg <pinkenburg@bnl.gov>
2023-03-07 12:03:36 -0500

    adjust tpc to use multiple servers

--- macros/run_tpc_client.C
+++ macros/run_tpc_client.C
@@ -11,36 +11,36 @@ void tpcDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
   // register histos we want with monitor name
-  cl->registerHisto("tpcmon_hist1", "TPCMON");
-  cl->registerHisto("tpcmon_hist2", "TPCMON");
-  cl->registerHisto("NorthSideADC", "TPCMON");
-  cl->registerHisto("SouthSideADC", "TPCMON");
+  cl->registerHisto("tpcmon_hist1", "TPCMON_0");
+  cl->registerHisto("tpcmon_hist2", "TPCMON_0");
+  cl->registerHisto("NorthSideADC", "TPCMON_0");
+  cl->registerHisto("SouthSideADC", "TPCMON_0");
   cl->AddServerHost("localhost");  // check local host first
   CreateHostList(online);
   // get my histos from server, the second parameter = 1
   // says I know they are all on the same node
-  cl->requestHistoBySubSystem("TPCMON", 1);
-  OnlMonDraw *tpcmon = new TpcMonDraw();  // create Drawing Object
+  cl->requestHistoBySubSystem("TPCMON_0", 1);
+  OnlMonDraw *tpcmon = new TpcMonDraw("TPCMONDRAW");  // create Drawing Object
   cl->registerDrawer(tpcmon);             // register with client framework
 }
 
 void tpcDraw(const char *what = "ALL")
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->requestHistoBySubSystem("TPCMON");        // update histos
-  cl->Draw("TPCMON", what);                     // Draw Histos of registered Drawers
+  cl->requestHistoBySubSystem("TPCMON_0");        // update histos
+  cl->Draw("TPCMONDRAW", what);                     // Draw Histos of registered Drawers
 }
 
 void tpcPS()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakePS("TPCMON");                         // Create PS files
+  cl->MakePS("TPCMONDRAW");                         // Create PS files
   return;
 }
 
 void tpcHtml()
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakeHtml("TPCMON");                       // Create html output
+  cl->MakeHtml("TPCMONDRAW");                       // Create html output
   return;
 }
