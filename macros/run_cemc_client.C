2937f229abffb97cf1f755cbb86bfaaa8c0d2f83
mchiu-bnl <chiu@bnl.gov>
2023-05-18 13:54:56 -0400

    prep to run on seb00

--- macros/run_cemc_client.C
+++ macros/run_cemc_client.C
@@ -11,29 +11,28 @@ void cemcDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
   // register histos we want with monitor name
-
-  cl->registerHisto("h2_cemc_rm", Form("CEMCMON_%d",online));
-  cl->registerHisto("h2_cemc_mean", Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_event", Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_waveform_twrAvg", Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_waveform_time", Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_waveform_pedestal", Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_fitting_sigDiff",Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_fitting_pedDiff",Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_fitting_timeDiff",Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_packet_number",Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_packet_length",Form("CEMCMON_%d",online));
-  cl->registerHisto("h1_packet_chans",Form("CEMCMON_%d",online));
+  int serverid = 0;
+  cl->registerHisto("h2_cemc_rm", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h2_cemc_mean", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_event", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_waveform_twrAvg", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_waveform_time", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_waveform_pedestal", Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_fitting_sigDiff",Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_fitting_pedDiff",Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_fitting_timeDiff",Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_packet_number",Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_packet_length",Form("CEMCMON_%d",serverid));
+  cl->registerHisto("h1_packet_chans",Form("CEMCMON_%d",serverid));
 
 		    
 for (int ih=0; ih<32; ih++){
-    cl->registerHisto(Form("h1_rm_sectorAvg_s%d",ih), Form("CEMCMON_%d",online));
+    cl->registerHisto(Form("h1_rm_sectorAvg_s%d",ih), Form("CEMCMON_%d",serverid));
   }
-  cl->AddServerHost("localhost");  // check local host first
-  // CreateHostList(online);
+  CreateHostList(online);
   //  get my histos from server, the second parameter = 1
   //  says I know they are all on the same node
-  cl->requestHistoBySubSystem(Form("CEMCMON_%d",online), 1);
+  cl->requestHistoBySubSystem(Form("CEMCMON_%d",serverid), 1);
   CemcMonDraw *cemcmon = new CemcMonDraw("CEMCMONDRAW");  // create Drawing Object
   cemcmon -> setSave(0);
   cl->registerDrawer(cemcmon);              // register with client framework
