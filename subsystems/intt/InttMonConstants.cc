0fa9fa5efe5a29639d6a5cb9f74b7b7c3c85eb91
josephbertaux <jbertau@purdue.edu>
2023-05-19 11:46:03 -0400

    Merged working changes from InttMonDraw rewrite

---
+++ subsystems/intt/InttMonConstants.cc
@@ -0,0 +1,53 @@
+#include "InttMonConstants.h"
+
+void INTT::GetBinFromIndexes(int& b, struct Indexes_s const& indexes)
+{
+	int s = 1;
+	b = 0;
+
+	b += indexes.adc * s;
+	s *= ADC;
+
+	b += indexes.chn * s;
+	s *= CHANNEL;
+
+	b += indexes.chp * s;
+	s *= CHIP;
+
+	b += indexes.arm * s;
+	s *= ARM;
+
+	for(int i = 0; i < indexes.lyr; ++i)
+	{
+		b += LADDER[i] * s;
+	}
+	b += indexes.ldr * s;
+
+	++b;
+}
+
+void INTT::GetIndexesFromBin(int b, struct Indexes_s& indexes)
+{
+	--b;
+
+	indexes.adc = b % ADC;
+	b /= ADC;
+
+	indexes.chn = b % CHANNEL;
+	b /= CHANNEL;
+
+	indexes.chp = b % CHIP;
+	b /= CHIP;
+
+	indexes.arm = b % ARM;
+	b /= ARM;
+
+	indexes.lyr = 0;	
+	while(b >= LADDER[indexes.lyr])
+	{
+		b -= LADDER[indexes.lyr];
+		++indexes.lyr;
+	}
+
+	indexes.ldr = b;
+}
