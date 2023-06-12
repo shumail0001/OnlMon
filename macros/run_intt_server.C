6a8cf5ed677e14e11825aa2dd6baeb7a5b2fdeb8
Chris Pinkenburg <pinkenburg@users.noreply.github.com>
2023-06-12 11:16:00 -0400

    Update run_intt_server.C
    Please no more changes for this macro. Feel free to change it in your private area, but do not check it in

--- macros/run_intt_server.C
+++ macros/run_intt_server.C
@@ -12,35 +12,11 @@ std::string _prdf_file = "/sphenix/u/jbertaux/evt_files/intt_intt0-00000025-0000
 
 void run_intt_server(const std::string &name = "INTTMON", unsigned int serverid = 0, const std::string &prdffile = _prdf_file)
 {
-//  //for debugging
-//  InttMon *m = new InttMon(name);
-//  m->MiscDebug();
-
-  std::cout << "name:\t" << name << std::endl;
-  std::cout << "Server ID:\t" << serverid << std::endl;
-  std::cout << "Using .evt file:" << std::endl;
-  std::cout << "\t" << prdffile << std::endl;
-
   OnlMon *m = new InttMon(name);      // create subsystem Monitor object
   m->SetMonitorServerId(serverid);
-  //m->AddTrigger("ONLMONBBCLL1"); // generic bbcll1 minbias trigger (defined in ServerFuncs.C)
+ 
   OnlMonServer *se = OnlMonServer::instance(); // get pointer to Server Framework
   se->registerMonitor(m);       // register subsystem Monitor with Framework
   start_server(prdffile);
-
-  //**********************************************************
-  //
-  // just a comment: This is not how this will be run
-  //
-  //**********************************************************
-  //m->Init(); this must already be called somewhere above
-  //m->BeginRun(1);
-
-  pidentify(0);
-  prun(2);
-
-  //for debugging
-  //static_cast<InttMon*>(m)->RandomEvent(serverid);
-
   return;
 }
