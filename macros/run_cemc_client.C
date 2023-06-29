17976a116085f22f6f57de6d4d91b8f1998b3d70
ahodges21 <ahodges4@illinois.edu>
2023-06-29 16:04:23 -0400

    duplicate requestHistoBySubsystem removed

--- macros/run_cemc_client.C
+++ macros/run_cemc_client.C
@@ -10,6 +10,7 @@ R__LOAD_LIBRARY(libonlcemcmon_client.so)
 void cemcDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
+  cl -> Verbosity(0);
   // register histos we want with monitor name
   for (int serverid = 0; serverid <= 7; serverid++)
     {
@@ -35,10 +36,10 @@ void cemcDrawInit(const int online = 0)
   //  get my histos from server, the second parameter = 1
   //  says I know they are all on the same node
   //cl->requestHistoBySubSystem(Form("CEMCMON_%d",serverid), 1);
-  for(int i = 0; i <= 7; i++)
-    {
-      cl->requestHistoBySubSystem(Form("CEMCMON_%d",i), 1);
-    }
+  // for(int i = 0; i <= 7; i++)
+  //   {
+  //     cl->requestHistoBySubSystem(Form("CEMCMON_%d",i), 1);
+  //   }
   CemcMonDraw *cemcmon = new CemcMonDraw("CEMCMONDRAW");  // create Drawing Object
   cemcmon -> setSave(0);
   cl->registerDrawer(cemcmon);              // register with client framework
