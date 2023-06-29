31c7e6a7d647cb54f1a81994405ad938497c01ff
ahodges21 <ahodges4@illinois.edu>
2023-06-29 16:04:59 -0400

    Updates, mostly to run over multiple SEB's

--- subsystems/cemc/CemcMonDraw.h
+++ subsystems/cemc/CemcMonDraw.h
@@ -45,6 +45,7 @@ class CemcMonDraw : public OnlMonDraw
   TPad *warning[18] = {nullptr};
   TGraphErrors *gr[2] = {nullptr};
   TStyle* cemcStyle = nullptr;
+  const int nSEBs = 8;
 };
 
 #endif /* CEMC_CEMCMONDRAW_H */
