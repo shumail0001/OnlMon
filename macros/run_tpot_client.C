7d531c663fa7823954d2e6b74101b74fcfd1df68
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-07-03 12:30:43 -0400

    updated Drawer name

--- macros/run_tpot_client.C
+++ macros/run_tpot_client.C
@@ -13,25 +13,25 @@ void tpotDrawInit(const int online = 0)
   auto cl = OnlMonClient::instance();
 
   // create drawing object and register
-  auto tpotmon = new TpotMonDraw("TPOTMONDRAW");
-  cl->registerDrawer(tpotmon);       
-  
+  auto tpotmon = new TpotMonDraw("TPOT");
+  cl->registerDrawer(tpotmon);
+
   // get detector names
   const auto detector_names = tpotmon->get_detnames_sphenix();
 
   // register histograms
   for( const std::string& hname: { "m_counters", "m_detector_occupancy_phi", "m_detector_occupancy_z", "m_resist_occupancy_phi", "m_resist_occupancy_z" } )
-  { cl->registerHisto( hname, "TPOTMON_0" ); } 
-  
+  { cl->registerHisto( hname, "TPOTMON_0" ); }
+
   for( const std::string& hname: { "m_adc_sample", "m_counts_sample", "m_hit_charge", "m_hit_multiplicity", "m_hit_vs_channel" } )
   {
     for( const auto& detname : detector_names )
     { cl->registerHisto( hname+"_"+detname, "TPOTMON_0" ); }
   }
-  
+
   // list of hosts from where histograms should be retrieved
   CreateHostList(online);
-  
+
   // get my histos from server, the second parameter = 1
   // says I know they are all on the same node
   cl->requestHistoBySubSystem("TPOTMON_0", 1);
@@ -39,21 +39,13 @@ void tpotDrawInit(const int online = 0)
 
 void tpotDraw(const char *what = "ALL")
 {
-  auto cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->requestHistoBySubSystem("TPOTMON_0",1);       // update histos
-  cl->Draw("TPOTMONDRAW", what);                    // Draw Histos of registered Drawers
+  auto cl = OnlMonClient::instance();
+  cl->requestHistoBySubSystem("TPOTMON_0",1);
+  cl->Draw("TPOT", what);
 }
 
 void tpotPS()
-{
-  auto cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakePS("TPOTMONDRAW");                        // Create PS files
-  return;
-}
+{ OnlMonClient::instance()->MakePS("TPOT"); }
 
 void tpotHtml()
-{
-  auto cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->MakeHtml("TPOTMONDRAW");                      // Create html output
-  return;
-}
+{ OnlMonClient::instance()->MakeHtml("TPOT"); }
