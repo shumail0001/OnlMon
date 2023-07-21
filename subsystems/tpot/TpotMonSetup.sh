68633475a4c7292849a33ed5195b1a082fdb147f
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-07-21 10:17:30 -0400

    Added TPOTCALIBREF env variable where reference histograms are expected to be found

--- subsystems/tpot/TpotMonSetup.sh
+++ subsystems/tpot/TpotMonSetup.sh
@@ -1,1 +1,11 @@
+#! /bin/bash
+
+
+if [[ -z "$TPOTCALIBREF" ]]
+then
+  export TPOTCALIBREF=/home/phnxrc/operations/TPOT/onlmon_ref
+  echo "TPOTCALIBREF environment variable set to $TPOTCALIBREF"
+  return
+fi
+
 export TPOTCALIB=$ONLMON_CALIB/tpot
