f7be02868d3e389b2377c8a1bc958fb24b604ac8
Chris Pinkenburg <pinkenburg@users.noreply.github.com>
2023-07-24 16:36:34 -0400

    Update TpotMonSetup.sh

--- subsystems/tpot/TpotMonSetup.sh
+++ subsystems/tpot/TpotMonSetup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-
+# no echo - it interferes with the monitor watcher
 if [[ -z "$TPOTCALIBREF" ]]
 then
   export TPOTCALIBREF=/home/phnxrc/operations/TPOT/onlmon_ref
-  echo "TPOTCALIBREF environment variable set to $TPOTCALIBREF"
+#  echo "TPOTCALIBREF environment variable set to $TPOTCALIBREF"
   return
 fi
 
