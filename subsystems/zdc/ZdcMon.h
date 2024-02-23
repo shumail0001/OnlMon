0992248503636b6c747fd880ac5af9ee76751fb5
Manuel Ramírez García <manuelramirezgarcia@MacBook-Air-232.lan>
2024-02-23 10:11:00 -0500

    I put functions, tuples, vectors and histograms that are in the cc file, into the header file.

--- subsystems/zdc/ZdcMon.h
+++ subsystems/zdc/ZdcMon.h
@@ -33,10 +33,42 @@ class ZdcMon : public OnlMon
   int idummy = 0;
   TH1 *zdc_adc_north = nullptr;
   TH1 *zdc_adc_south = nullptr;
+  // smd
+  // north smd
+  TH1 *smd_hor_north = nullptr;
+  TH1 *smd_ver_north = nullptr;
+  TH1 *smd_sum_hor_north = nullptr;
+  TH1 *smd_sum_ver_north = nullptr;
+  // south smd 
+  TH1 *smd_hor_south
+  TH1 *smd_ver_south = nullptr;
+  TH1 *smd_sum_hor_south = nullptr;
+  TH1 *smd_sum_ver_south = nullptr;
+  // smd values
+  TH1 *smd_value = nullptr;
+  TH1 *smd_value_good = nullptr;
+  TH1 *smd_value_small = nullptr;
 
+  void GetCalConst();
   void CompSmdAdc();
   void CompSmdPos();
   void CompSumSmd();
+
+
+
+  float smd_adc[32];
+  float zdc_adc[8];
+  float smd_sum[4]; 
+  float smd_pos[4];
+
+  float pedestal[40];
+  float zdc_ped[8];
+  float smd_ped[32];
+  float gain[32];
+  float smd_south_rgain[16];
+  float smd_north_rgain[16];
+  float overflow0[40];
+  float overflow1[40];
 
 };
 
