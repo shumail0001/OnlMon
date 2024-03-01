e6e4d6109dac45a94954071b84060e3642fba608
Manuel Ramírez García <manuelramirezgarcia@MacBook-Air-232.local>
2024-03-01 09:13:51 -0500

    fixed histogram in the header file.

--- subsystems/zdc/ZdcMon.h
+++ subsystems/zdc/ZdcMon.h
@@ -44,7 +44,7 @@ class ZdcMon : public OnlMon
   TH1 *smd_hor_north_good = nullptr;
   TH1 *smd_ver_north_good = nullptr;
   // south smd 
-  TH1 *smd_hor_south;
+  TH1 *smd_hor_south nullptr;
   TH1 *smd_ver_south = nullptr;
   TH1 *smd_sum_hor_south = nullptr;
   TH1 *smd_sum_ver_south = nullptr;
