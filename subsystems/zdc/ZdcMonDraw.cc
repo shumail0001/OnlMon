1c493566bedf6195a2ead7dc9af926918c1e046b
Manuel Ramírez García <manuelramirezgarcia@MacBook-Air-232.physics.lsa.umich.edu>
2024-03-29 17:14:32 -0400

    null pointers for the histos.

--- subsystems/zdc/ZdcMonDraw.cc
+++ subsystems/zdc/ZdcMonDraw.cc
@@ -490,15 +490,15 @@ int ZdcMonDraw::DrawSmdAdcNorthIndividual(const std::string & /* what */)
   OnlMonClient *cl = OnlMonClient::instance();
 
   // Array that holds pointer to the histogram of each channel
-  // TH1* smd_adc_n_hor_ind[8];
+  TH1* smd_adc_n_hor_ind[8] = {nullptr};
   for (int i = 0; i < 8; ++i) 
   {
     char histName[256]; // string
-    sprintf(histName, "smd_adc_n_hor_ind%d", i); // Create the histogram name with the loop index 'i'
+    sprintf(histName, "smd_adc_n_hor_ind%d", i); // Name of the histogram name with the loop index 'i'
     smd_adc_n_hor_ind[i] = cl->getHisto("ZDCMON_0", histName); // Retrieve histogram pointer using 'histName'
   }
   // Array that holds pointer to the histogram of each channel
-  // TH1* smd_adc_n_ver_ind[7];
+  TH1* smd_adc_n_ver_ind[7] = {nullptr};
   for (int i = 0; i < 7; ++i) 
   {
     char histName[256]; // string
