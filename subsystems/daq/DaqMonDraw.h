b948097aa8bbe7cf524122716fa2afef969b7948
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-05-10 09:22:04 -0400

    clang-tidy and clang-format for daq

--- subsystems/daq/DaqMonDraw.h
+++ subsystems/daq/DaqMonDraw.h
@@ -4,8 +4,8 @@
 #include <onlmon/OnlMonDraw.h>
 
 #include <TH2.h>
-#include <TStyle.h>
 #include <TLatex.h>
+#include <TStyle.h>
 #include <string>  // for allocator, string
 
 class OnlMonDB;
@@ -22,8 +22,8 @@ class DaqMonDraw : public OnlMonDraw
 
   int Init() override;
   int Draw(const std::string &what = "ALL") override;
- int MakeHtml(const std::string &what = "ALL") override;
- int SavePlot(const std::string &what = "ALL", const std::string &type = "png") override;
+  int MakeHtml(const std::string &what = "ALL") override;
+  int SavePlot(const std::string &what = "ALL", const std::string &type = "png") override;
 
  protected:
   int MakeCanvas(const std::string &name);
@@ -31,14 +31,14 @@ class DaqMonDraw : public OnlMonDraw
   int DrawSecond(const std::string &what = "ALL");
   int DrawServerStats();
   time_t getTime();
-//  int DrawHistory(const std::string &what = "ALL");
+  //  int DrawHistory(const std::string &what = "ALL");
   int TimeOffsetTicks = -1;
   TCanvas *TC[3] = {nullptr};
   TPad *transparent[3] = {nullptr};
   TPad *Pad[6] = {nullptr};
   TGraphErrors *gr[2] = {nullptr};
   OnlMonDB *dbvars = nullptr;
-  TStyle* daqStyle = nullptr;
+  TStyle *daqStyle = nullptr;
 };
 
 #endif /* DAQ_DAQMONDRAW_H */
