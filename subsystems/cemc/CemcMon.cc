912a88a2270a35b091e5ad05ff0d8c10ceae2e8e
Shuhang Li <71942661+Shuonli@users.noreply.github.com>
2024-05-30 16:36:18 -0400

    emcal rm +1 event number for trigger vector

--- subsystems/cemc/CemcMon.cc
+++ subsystems/cemc/CemcMon.cc
@@ -323,7 +323,7 @@ int CemcMon::process_event(Event *e /* evt */)
   if (anaGL1)
   {
     int evtnr = e->getEvtSequence();
-    Event *gl1Event = erc->getEvent(evtnr+1);
+    Event *gl1Event = erc->getEvent(evtnr);
     if (gl1Event)
     {
       have_gl1 = true;
