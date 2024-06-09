6be6228ae7fd7acf699b39ca327c202367eb6bde
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2024-06-09 00:05:23 -0400

    added n_waveform_vs_channels histograms.

--- subsystems/tpot/TpotMonDraw.cc
+++ subsystems/tpot/TpotMonDraw.cc
@@ -447,6 +447,18 @@ TCanvas* TpotMonDraw::create_canvas(const std::string &name)
     m_canvas.push_back( cv );
     return cv;
 
+  } else if (name == "TPOT_waveform_vs_channel") {
+
+    auto cv = new TCanvas(name.c_str(), "TpotMon waveform vs channel", -1, 0, xsize / 2, ysize);
+    gSystem->ProcessEvents();
+    divide_canvas(cv, 4, 4);
+    hide_margins(cv,0.2);
+    create_transparent_pad(name);
+    cv->SetEditable(false);
+    m_canvas.push_back( cv );
+    return cv;
+
+
   } else if (name == "TPOT_hit_vs_channel") {
 
     auto cv = new TCanvas(name.c_str(), "TpotMon hit vs channel", -1, 0, xsize / 2, ysize);
@@ -458,8 +470,7 @@ TCanvas* TpotMonDraw::create_canvas(const std::string &name)
     m_canvas.push_back( cv );
     return cv;
 
-  }
-  return nullptr;
+  }  return nullptr;
 }
 
 //_______________________________________________________________________________
@@ -639,6 +650,35 @@ int TpotMonDraw::Draw(const std::string &what)
   if (what == "ALL" || what == "TPOT_hit_multiplicity")
   {
     iret += draw_array("TPOT_hit_multiplicity", get_histograms( "m_hit_multiplicity" ), get_ref_histograms_scaled( "m_hit_multiplicity" ), DrawOptions::Logy|DrawOptions::MatchRange );
+    ++idraw;
+  }
+
+  if (what == "ALL" || what == "TPOT_waveform_vs_channel")
+  {
+    iret += draw_array("TPOT_waveform_vs_channel", get_histograms( "m_wf_vs_channel" ), get_ref_histograms_scaled( "m_wf_vs_channel" ), DrawOptions::Logy|DrawOptions::MatchRange|DrawOptions::Normalize);
+    auto cv = get_canvas("TPOT_waveform_vs_channel");
+    if( cv )
+    {
+      CanvasEditor cv_edit(cv);
+      cv->Update();
+      for( int i = 0; i < MicromegasDefs::m_nfee; ++i )
+      {
+        // draw vertical lines that match HV sectors
+        // also set log y
+        auto&& pad = cv->GetPad(i+1);
+        pad->cd();
+        pad->Update();
+        for( const int& channel:{64, 128, 196} )
+        {
+          const auto line = vertical_line( pad, channel );
+          line->SetLineStyle(2);
+          line->SetLineColor(1);
+          line->SetLineWidth(1);
+          line->Draw();
+        }
+      }
+    }
+
     ++idraw;
   }
 
