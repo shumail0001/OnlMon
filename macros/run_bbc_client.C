e7252d6e7537739534c9731be0d1ebba31f06bfc
mchiu-bnl <chiu@bnl.gov>
2024-07-13 13:04:45 -0400

    Add gl1 counter to bbc serverstats

--- macros/run_bbc_client.C
+++ macros/run_bbc_client.C
@@ -14,62 +14,68 @@ void bbcDrawInit(const int online = 0)
 {
   OnlMonClient *cl = OnlMonClient::instance();
   // register histos we want with monitor name
-  cl->Verbosity(100);
-  cl->registerHisto("bbc_south_nhit", "BBCMON_0");
-  cl->registerHisto("bbc_north_nhit", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_emcal0", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_hcal0", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_emcalmbd0", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_hcalmbd0", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_emcal1", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_hcal1", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_emcalmbd1", "BBCMON_0");
-  cl->registerHisto("bbc_nhit_hcalmbd1", "BBCMON_0");
-  cl->registerHisto("bbc_adc", "BBCMON_0");
-  cl->registerHisto("bbc_tdc", "BBCMON_0");
-  cl->registerHisto("bbc_tdc_overflow", "BBCMON_0");
-  cl->registerHisto("bbc_tdc_armhittime", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_ns", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_10", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_30", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_60", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_10_chk", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_30_chk", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_60_chk", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_zdcns", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_emcal", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_hcal", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_emcalmbd", "BBCMON_0");
-  cl->registerHisto("bbc_zvertex_hcalmbd", "BBCMON_0");
-  cl->registerHisto("bbc_nevent_counter", "BBCMON_0");
-  cl->registerHisto("bbc_tzero_zvtx", "BBCMON_0");
-  cl->registerHisto("bbc_prescale_hist", "BBCMON_0");
-  cl->registerHisto("bbc_trigs", "BBCMON_0");
-  cl->registerHisto("bbc_avr_hittime", "BBCMON_0");
-  cl->registerHisto("bbc_north_hittime", "BBCMON_0");
-  cl->registerHisto("bbc_south_hittime", "BBCMON_0");
-  cl->registerHisto("bbc_north_chargesum", "BBCMON_0");
-  cl->registerHisto("bbc_south_chargesum", "BBCMON_0");
-  cl->registerHisto("bbc_time_wave", "BBCMON_0");
-  cl->registerHisto("bbc_charge_wave", "BBCMON_0");
-  cl->registerHisto("bbc_south_hitmap", "BBCMON_0");
-  cl->registerHisto("bbc_north_hitmap", "BBCMON_0");
+  //  cl->Verbosity(100);
+  OnlMonDraw *bbcmon = new BbcMonDraw("BBCMONDRAW");  // create Drawing Object
+  std::string servername = "BBCMON_0";
+  bbcmon->AddServer(servername);
+  cl->registerHisto("bbc_south_nhit", servername);
+  cl->registerHisto("bbc_north_nhit", servername);
+  cl->registerHisto("bbc_nhit_emcal0", servername);
+  cl->registerHisto("bbc_nhit_hcal0", servername);
+  cl->registerHisto("bbc_nhit_emcalmbd0", servername);
+  cl->registerHisto("bbc_nhit_hcalmbd0", servername);
+  cl->registerHisto("bbc_nhit_emcal1", servername);
+  cl->registerHisto("bbc_nhit_hcal1", servername);
+  cl->registerHisto("bbc_nhit_emcalmbd1", servername);
+  cl->registerHisto("bbc_nhit_hcalmbd1", servername);
+  cl->registerHisto("bbc_adc", servername);
+  cl->registerHisto("bbc_tdc", servername);
+  cl->registerHisto("bbc_tdc_overflow", servername);
+  cl->registerHisto("bbc_tdc_armhittime", servername);
+  cl->registerHisto("bbc_zvertex", servername);
+  cl->registerHisto("bbc_zvertex_ns", servername);
+  cl->registerHisto("bbc_zvertex_10", servername);
+  cl->registerHisto("bbc_zvertex_30", servername);
+  cl->registerHisto("bbc_zvertex_60", servername);
+  cl->registerHisto("bbc_zvertex_10_chk", servername);
+  cl->registerHisto("bbc_zvertex_30_chk", servername);
+  cl->registerHisto("bbc_zvertex_60_chk", servername);
+  cl->registerHisto("bbc_zvertex_zdcns", servername);
+  cl->registerHisto("bbc_zvertex_emcal", servername);
+  cl->registerHisto("bbc_zvertex_hcal", servername);
+  cl->registerHisto("bbc_zvertex_emcalmbd", servername);
+  cl->registerHisto("bbc_zvertex_hcalmbd", servername);
+  cl->registerHisto("bbc_nevent_counter", servername);
+  cl->registerHisto("bbc_tzero_zvtx", servername);
+  cl->registerHisto("bbc_prescale_hist", servername);
+  cl->registerHisto("bbc_trigs", servername);
+  cl->registerHisto("bbc_avr_hittime", servername);
+  cl->registerHisto("bbc_north_hittime", servername);
+  cl->registerHisto("bbc_south_hittime", servername);
+  cl->registerHisto("bbc_north_chargesum", servername);
+  cl->registerHisto("bbc_south_chargesum", servername);
+  cl->registerHisto("bbc_time_wave", servername);
+  cl->registerHisto("bbc_charge_wave", servername);
+  cl->registerHisto("bbc_south_hitmap", servername);
+  cl->registerHisto("bbc_north_hitmap", servername);
 
   // for local host, just call bbcDrawInit(2)
   //cl->AddServerHost("localhost");  // check local host first
   CreateSubsysHostlist("bbc_hosts.list", online);
 
   // says I know they are all on the same node
-  cl->requestHistoBySubSystem("BBCMON_0", 1);
-  OnlMonDraw *bbcmon = new BbcMonDraw("BBCMONDRAW");  // create Drawing Object
+  cl->requestHistoBySubSystem(servername, 1);
   cl->registerDrawer(bbcmon);             // register with client framework
 }
 
 void bbcDraw(const char *what = "ALL")
 {
   OnlMonClient *cl = OnlMonClient::instance();  // get pointer to framewrk
-  cl->requestHistoBySubSystem("BBCMON_0",1);        // update histos
+  OnlMonDraw *bbcdraw = cl->GetDrawer("BBCMONDRAW");
+  for (auto iter = bbcdraw->ServerBegin(); iter != bbcdraw->ServerEnd(); ++iter)
+  {
+    cl->requestHistoBySubSystem(iter->c_str(), 1);
+  }
   cl->Draw("BBCMONDRAW", what);                     // Draw Histos of registered Drawers
 }
 
