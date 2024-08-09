01fdd0db922abc91a079613dd75d226f26cda389
chughes90 <chughe26@vols.utk.edu>
2024-08-09 17:18:44 -0400

    Now that we know where the laser peak is in time in the new gas, adjusting the peak finder

--- subsystems/tpc/TpcMon.cc
+++ subsystems/tpc/TpcMon.cc
@@ -1050,7 +1050,7 @@ int TpcMon::process_event(Event *evt/* evt */)
 
           if( adc > wf_max){ wf_max = adc; t_max = s; pedest_sub_wf_max = adc - pedestal;}
 
-          if( (s> 410 && s < 422) && (adc > wf_max_laser_peak) ){ wf_max_laser_peak = adc; pedest_sub_wf_max_laser_peak = adc - pedestal; }   
+          if( (s> 440 && s < 452) && (adc > wf_max_laser_peak) ){ wf_max_laser_peak = adc; pedest_sub_wf_max_laser_peak = adc - pedestal; }   
 
           if( (store_ten.size() < 10) ) // get first 10
           {
